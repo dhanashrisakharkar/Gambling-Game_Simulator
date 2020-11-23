62b956a784aad1a0e481680ebdd1e41455ccc312
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-11-23 16:00:53 +0530

    Added : Random Winner

--- Gambling-Game_Simulation/src/gambling_simulator/GamblingSimulator.java
+++ Gambling-Game_Simulation/src/gambling_simulator/GamblingSimulator.java
@@ -1,13 +1,32 @@
 package gambling_simulator;
 
+import java.util.Random;
+
 public class GamblingSimulator {
-	public static int stack =100;
-	public static int bet = 1;
+	public static final int stack =100;
+	public static final int bet = 1;
+	public static int  Win = 0;
+	public static int Loose = 0;
+	
+	public void checkWinningLoose(){
+		  Random No = new Random();
+		  int randomNo = No.nextInt(2);
+		  if(randomNo == 0)
+		  {
+			  Win = bet;
+			  System.out.println("Gambler Is Winner"+Win);
+		  }else{
+			  Loose=bet;
+			  System.out.println("Gamler is not winner"+Loose);
+		  }
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Welcome To Gambling Simulator");
-       System.out.println(stack+" "+ bet);
+       GamblingSimulator gamblingSimulator = new GamblingSimulator();
+       gamblingSimulator.checkWinningLoose();
+       
 	}
-
+  
 }
