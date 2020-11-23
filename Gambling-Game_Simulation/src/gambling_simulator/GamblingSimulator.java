2ba26fb30e71b4e843605304be4269ab1b6f6725
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-11-23 16:30:05 +0530

    Added : Range Of Loose Or Winner

--- Gambling-Game_Simulation/src/gambling_simulator/GamblingSimulator.java
+++ Gambling-Game_Simulation/src/gambling_simulator/GamblingSimulator.java
@@ -20,12 +20,33 @@ public class GamblingSimulator {
 			  System.out.println("Gamler is not winner"+Loose);
 		  }
 	}
+	public void WinningLooseRange(){
+		  Random No = new Random();
+		
+		  int tempstack = stack;
+		  while (tempstack>50 && tempstack<150){
+			  int randomNo = No.nextInt(2); 
+		  
+		  if(randomNo == 0)
+		  {
+			  tempstack=tempstack+bet;
+			  Win = bet;
+			  System.out.println("Gambler Is Winner"+Win);
+		  }else{
+			  tempstack=tempstack-bet;
+			  Loose=bet;
+			  System.out.println("Gamler is not winner"+Loose);
+		  }
+	}
+		  System.out.println("Total Stack is : "+tempstack);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Welcome To Gambling Simulator");
        GamblingSimulator gamblingSimulator = new GamblingSimulator();
        gamblingSimulator.checkWinningLoose();
+       gamblingSimulator.WinningLooseRange();
        
 	}
   
