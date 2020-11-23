882407b3699d96a1a9f7534798a2ad10f25e684f
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-11-23 17:46:53 +0530

    Added : Total Earning in month

--- Gambling-Game_Simulation/src/gambling_simulator/GamblingSimulator.java
+++ Gambling-Game_Simulation/src/gambling_simulator/GamblingSimulator.java
@@ -7,6 +7,8 @@ public class GamblingSimulator {
 	public static final int bet = 1;
 	public static int Win = 0;
 	public static int Loose = 0;
+	public static int TempStack = 50;
+	public static int Total_Amount = 0;
 
 	public void checkWinningLoose() {
 		Random No = new Random();
@@ -20,7 +22,7 @@ public class GamblingSimulator {
 		}
 	}
 
-	public void WinningLooseRange() {
+	public int WinningLooseRange() {
 		Random No = new Random();
 
 		int tempstack = stack;
@@ -38,6 +40,21 @@ public class GamblingSimulator {
 			}
 		}
 		System.out.println("Total Stack is : " + tempstack);
+		return tempstack;
+	}
+
+	public void TotalAmountWinorLoss(int day) {
+		for (int index = 0; index <= day; index++) {
+			int temp = WinningLooseRange();
+			if (temp - stack > 0) {
+				Total_Amount = Total_Amount + (TempStack - stack);
+				System.out.println("winnning day " + index + "Amount is" + Total_Amount);
+			} else {
+				Total_Amount = Total_Amount + (stack - TempStack);
+				System.out.println("loosing days" + index + "Amount is " + Total_Amount);
+			}
+		}
+
 	}
 
 	public static void main(String[] args) {
@@ -46,6 +63,7 @@ public class GamblingSimulator {
 		GamblingSimulator gamblingSimulator = new GamblingSimulator();
 		gamblingSimulator.checkWinningLoose();
 		gamblingSimulator.WinningLooseRange();
+		gamblingSimulator.TotalAmountWinorLoss(20);
 
 	}
 
