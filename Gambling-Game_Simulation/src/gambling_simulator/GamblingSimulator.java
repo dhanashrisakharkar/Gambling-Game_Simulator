1d2f2b5e09afadf4b0f3eaf7ea169cf7595486ba
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-11-23 15:30:21 +0530

    Added : Add Two Variable

---
+++ Gambling-Game_Simulation/src/gambling_simulator/GamblingSimulator.java
@@ -0,0 +1,12 @@
+package gambling_simulator;
+
+public class GamblingSimulator {
+	int stack =100;
+	int bet = 1;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Welcome To Gambling Simulator");
+	}
+
+}
